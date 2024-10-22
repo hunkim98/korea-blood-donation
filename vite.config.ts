e339d2ad334b0ed892a37db9b782cb8d74698532
Hun Kim <hunkim98@gmail.com>
2024-10-22 01:57:29 -0400

    Fix basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ command }) => ({
-  plugins: [react()],
-  base: command === "build" ? "/korea-blood-donation/" : "/",
+  plugins: [react(), tsconfigPaths()],
+  base: "/korea-blood-donation/",
 }));
