21eed182eb7c76261a230af8dffe901323a740cb
Hun Kim <hunkim98@gmail.com>
2024-10-22 01:54:34 -0400

    Change config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  base: "/korea-blood-donation/",
-});
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === "build" ? "/korea-blood-donation/" : "/",
+}));
