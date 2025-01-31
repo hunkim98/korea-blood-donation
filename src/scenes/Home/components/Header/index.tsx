b2eaa015f1a5c4be4f8cab95636e1a38edd88f9b
Hun Kim <hunkim98@gmail.com>
2025-01-31 00:39:23 -0500

    Change paragraph

--- src/scenes/Home/components/Header/index.tsx
+++ src/scenes/Home/components/Header/index.tsx
@@ -12,13 +12,11 @@ const Header: FC = () => {
         <h6 className="text-body-large opacity-50">{`A Case Study of South Korea`}</h6>
         {/* </div> */}
         <div className="h-2" />
-        <p className="max-w-[700px] text-body-small">
-          When it comes to analyzing how blood donations are managed, it is
-          important to consider the balance between supply and demand. This
-          visualization explores the delicate balance of blood supply and demand
-          in South Korea. The supply data was calculated using the amount of
-          blood donations and the demand data was calculated using the amount of
-          surgeries operated in hospitals. All data was collected from{" "}
+        <p className="max-w-[700px] text-body-medium">
+          This visualization examines South Korea’s blood system by comparing
+          donation levels to surgical demand. The findings reveal clear seasonal
+          imbalances, with shortages in winter and surpluses in summer, shedding
+          light on critical gaps in donation trends. All data was collected from{" "}
           <a href="https://kosis.kr/eng/">
             <span className="underline">KOSIS</span>
           </a>
